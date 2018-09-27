7089491bb95c2f40bdfa98b8cae8f7fe3ecaebf3
Noelle Pablo <noellebpablo@gmail.com>
2018-09-27 12:19:37 -0500

    changed name of original num_word function, added documentation

---
+++ R/num2word.R
@@ -0,0 +1,25 @@
+#' Number converting function
+#'
+#' This function allows you to convert an integer from 0 to 9 to a text representation of the integer.
+#' @param x Ideally an integer from 0 to 9. If x is already a written number or any integer not in [0, 9], then this function will return x.
+#' @keywords convert integer text number
+#' @export
+#' @examples
+#' num2word(4)
+#' num2word("five")
+#' num2word(-99)
+
+num2word <- function(x) {
+  num_lookup <- data_frame(
+    number = 0:9,
+    word = c("zero", "one", "two", "three", "four", "five", "six", "seven",
+             "eight", "nine")
+  )
+  if (x %in% num_lookup$number) {
+    num_lookup %>%
+      filter(number == x) %>%
+      pull(word)
+  } else {
+    x
+  }
+}
