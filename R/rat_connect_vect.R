7b85dbec6276322d247583de83d584e0645383ca
Noelle Pablo <noellebpablo@gmail.com>
2018-10-17 15:22:19 -0500

    added check_vec function

--- R/rat_connect_vect.R
+++ R/rat_connect_vect.R
@@ -2,7 +2,7 @@
 #'
 #' This function allows you to turn a vector of numeric, character or logical values into a character string.
 #' The last object in the vector will be connected to the rest of the objects with an "and" by default.
-#' @param vec a vector or list containing numeric, character or logical values.
+#' @param vec a vector or list containing character values.
 #' @param final A character object that connects the last object in vec to the rest of the objects in vec. Defaults to "and".
 #' @keywords paste concatenate integer text number
 #' @export
@@ -12,6 +12,9 @@
 #' rat_connect_vect(c("chicken", 2))
 
 rat_connect_vect <- function(vec, final = "and") {
+  vec <- check_vec(vec)
+  final <- check_vec(final)
+
   if (length(vec) == 1) {
     return(vec)
   } else if (length(vec) == 2) {
@@ -21,3 +24,12 @@ rat_connect_vect <- function(vec, final = "and") {
     sub(",\\s+([^,]+)$", " \\1", toString(vec))
   }
 }
+
+check_vec <- function(x) {
+  if (!is.character(x)){
+    stop("`x` must be a character vector.", call. = FALSE)
+  } else {
+    x
+  }
+}
+
