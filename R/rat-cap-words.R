1fe0a3619a69183a5a783028791ec08c2c83dae9
Jake Thompson <wjakethompson@gmail.com>
2019-04-16 13:13:40 -0500

    add word capitalization

---
+++ R/rat-cap-words.R
@@ -0,0 +1,55 @@
+#' Capitalization of words
+#'
+#' @param x A character string
+#' @param all Logical. If `TRUE`, the first letter of every word is capitalized. If `FALSE` (the default), only the first word is capitalized.
+#'
+#' @return A character string with the specified capitalization.
+#' @export
+#'
+#' @examples
+#' name <- c("zip code", "state", "final count")
+#' vapply(name, rat_cap_words, character(1))
+#' vapply(name, rat_cap_words, character(1), all = TRUE)
+rat_cap_words <- function(x, all = FALSE) {
+  x <- check_cap_words(x)
+  all <- check_all(all)
+
+  if (all) {
+    s <- strsplit(x, " ")[[1]]
+    paste(toupper(substring(s, 1,1)), substring(s, 2),
+          sep="", collapse=" ")
+  } else {
+    s <- strsplit(x, " ")[[1]] %>% tolower()
+    s <- paste0(toupper(substring(s, 1, 1)), substring(s, 2))
+    if (length(s) > 1) {
+      s[2:length(s)] <- tolower(s[2:length(s)])
+    }
+    paste(s, sep = "", collapse = " ")
+  }
+}
+
+
+check_cap_words <- function(x) {
+  if (length(x) != 1 || !is.character(x)) {
+    stop("`x` must be a length one character vector.", call. = FALSE)
+  }
+
+  if (is.na(x)) {
+    stop("`x` must be non-missing.", call. = FALSE)
+  } else {
+    x
+  }
+}
+
+check_all <- function(x) {
+  if (length(x) != 1 || !is.logical(x)) {
+    stop("`all` must be a length one logical vector.", call. = FALSE)
+  }
+  x <- as.integer(x)
+
+  if (is.na(x)) {
+    stop("`all` must be non-missing.", call. = FALSE)
+  } else {
+    x
+  }
+}
---
+++ tests/testthat/test-rat-cap-words.R
@@ -0,0 +1,23 @@
+context("test-rat-cap-words")
+
+test_that("x must be a valid value", {
+  expect_error(check_cap_words(1), "length one character vector")
+  expect_error(check_cap_words(TRUE), "length one character vector")
+  expect_error(check_cap_words(c("a", "b", "c")), "length one character vector")
+  expect_error(check_cap_words(NA_character_), "non-missing")
+})
+
+test_that("all must be a valid value", {
+  expect_error(check_all("a"), "length one logical vector")
+  expect_error(check_all(1:10), "length one logical vector")
+  expect_error(check_all(NA), "non-missing")
+})
+
+test_that("words capitalize", {
+  expect_equal(rat_cap_words("state"), "State")
+  expect_equal(rat_cap_words("state", all = TRUE), "State")
+  expect_equal(rat_cap_words("zip code"), "Zip code")
+  expect_equal(rat_cap_words("zip code", all = TRUE), "Zip Code")
+  expect_equal(rat_cap_words("quick brown fox"), "Quick brown fox")
+  expect_equal(rat_cap_words("quick brown fox", all = TRUE), "Quick Brown Fox")
+})
