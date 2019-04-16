fa498c01dedbc392f54fc3694718c9a720e0c1ed
Jake Thompson <wjakethompson@gmail.com>
2019-04-16 13:18:00 -0500

    resolve lints

--- R/rat-cap-words.R
+++ R/rat-cap-words.R
@@ -16,8 +16,8 @@ rat_cap_words <- function(x, all = FALSE) {
 
   if (all) {
     s <- strsplit(x, " ")[[1]]
-    paste(toupper(substring(s, 1,1)), substring(s, 2),
-          sep="", collapse=" ")
+    paste(toupper(substring(s, 1, 1)), substring(s, 2),
+          sep = "", collapse = " ")
   } else {
     s <- strsplit(x, " ")[[1]] %>% tolower()
     s <- paste0(toupper(substring(s, 1, 1)), substring(s, 2))
