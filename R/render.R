f193e6097c976959a95b7b662b5d045e1183e84a
Jake Thompson <wjakethompson@gmail.com>
2019-04-16 12:52:04 -0500

    add functionality for word doc topic guide

---
+++ R/render.R
@@ -0,0 +1,30 @@
+#' Create an R Markdown Word Document Topic Guide
+#'
+#' This is a function called in the output of the YAML of the Rmd file to
+#' specify using the standard DLM topic guide word document formatting.
+#'
+#' @param ... Arguments to be passed to `[bookdown::word_document2]`
+#'
+#' @return A modified `word_document2` with the standard topic guide formatting.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   output: ratlas::topicguide_docx
+#' }
+topicguide_docx <- function(...) {
+  template <- find_resource("topicguide", "template.docx")
+  base <- bookdown::word_document2(reference_docx = template, ...)
+
+  base$knitr$opts_chunk$comment <- "#>"
+  base$knitr$opts_chunk$message <- FALSE
+  base$knitr$opts_chunk$warning <- FALSE
+  base$knitr$opts_chunk$error <- FALSE
+  base$knitr$opts_chunk$echo <- FALSE
+  base$knitr$opts_chunk$cache <- FALSE
+  base$knitr$opts_chunk$fig.ext <- "png"
+  base$knitr$opts_chunk$fig.retina <- 3
+  base$knitr$opts_chunk$fig.path <- "figures/"
+
+  base
+}
