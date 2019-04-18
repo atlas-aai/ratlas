30cc49197be8af2aa4b4986e9f11c42bc1291a20
Jake Thompson <wjakethompson@gmail.com>
2019-04-18 09:09:08 -0500

    don't add _bookdown.yml file to topicguide project

--- R/skeleton.R
+++ R/skeleton.R
@@ -15,10 +15,5 @@ topicguide_skeleton <- function(path) {
   target <- file.path(path, files)
   file.copy(source, target)
 
-  # add book_filename to _bookdown.yml and to the base path name
-  f <- file.path(path, "_bookdown.yml")
-  x <- read_utf8(f)
-  write_utf8(c(sprintf('book_filename: "%s"', basename(path)), x), f)
-
   TRUE
 }
