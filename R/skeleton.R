886b6daeadd2e796b3987fef4a0c25cf10c0a6a6
Jake Thompson <wjakethompson@gmail.com>
2019-04-18 09:05:50 -0500

    add fs dependency for subdirectory creation

--- R/skeleton.R
+++ R/skeleton.R
@@ -1,12 +1,15 @@
 topicguide_skeleton <- function(path) {
-  # ensure directory exists
-  dir.create(path, recursive = TRUE, showWarnings = FALSE)
+  # copy 'topicguide_resources' folder to path
+  resources <- ratlas_file("rstudio", "templates", "project",
+                           "topicguide_resources")
 
-  # copy 'resources' folder to path
-  resources <- ratlas_file("rstudio", "templates", "project", "topicguide",
-                           "resources")
-
+  sub_dirs <- list.dirs(resources, recursive = TRUE, full.names = FALSE)
+  sub_dirs <- sub_dirs[-which(sub_dirs == "")]
   files <- list.files(resources, recursive = TRUE, include.dirs = FALSE)
+
+  # ensure directories exists
+  new_path <- c(path, file.path(path, sub_dirs))
+  fs::dir_create(new_path)
 
   source <- file.path(resources, files)
   target <- file.path(path, files)
