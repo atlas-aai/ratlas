1f04e82d0883077932691d056e18dd026f234196
Noelle Pablo <noellebpablo@gmail.com>
2019-05-16 16:04:01 -0500

    add default continuous color scheme

--- R/zzz.R
+++ R/zzz.R
@@ -15,29 +15,29 @@
                        plot_margin = margin(30, 30, 30, 30),
                        grid_col = "#cccccc", grid = TRUE,
                        axis_col = "#cccccc", axis = FALSE, ticks = FALSE) {
-
+    
     ret <- ggplot2::theme_minimal(base_family=base_family, base_size=base_size)
-
+    
     ret <- ret + theme(legend.background=element_blank())
     ret <- ret + theme(legend.key=element_blank())
-
+    
     if (inherits(grid, "character") | grid == TRUE) {
-
+      
       ret <- ret + theme(panel.grid=element_line(color=grid_col, size=0.2))
       ret <- ret + theme(panel.grid.major=element_line(color=grid_col, size=0.2))
       ret <- ret + theme(panel.grid.minor=element_line(color=grid_col, size=0.15))
-
+      
       if (inherits(grid, "character")) {
         if (regexpr("X", grid)[1] < 0) ret <- ret + theme(panel.grid.major.x=element_blank())
         if (regexpr("Y", grid)[1] < 0) ret <- ret + theme(panel.grid.major.y=element_blank())
         if (regexpr("x", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.x=element_blank())
         if (regexpr("y", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.y=element_blank())
       }
-
+      
     } else {
       ret <- ret + theme(panel.grid=element_blank())
     }
-
+    
     if (inherits(axis, "character") | axis == TRUE) {
       ret <- ret + theme(axis.line=element_line(color="#2b2b2b", size=0.15))
       if (inherits(axis, "character")) {
@@ -59,7 +59,7 @@
     } else {
       ret <- ret + theme(axis.line=element_blank())
     }
-
+    
     if (!ticks) {
       ret <- ret + theme(axis.ticks = element_blank())
       ret <- ret + theme(axis.ticks.x = element_blank())
@@ -70,10 +70,10 @@
       ret <- ret + theme(axis.ticks.y = element_line(size=0.15))
       ret <- ret + theme(axis.ticks.length = grid::unit(5, "pt"))
     }
-
+    
     xj <- switch(tolower(substr(axis_title_just, 1, 1)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
     yj <- switch(tolower(substr(axis_title_just, 2, 2)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
-
+    
     ret <- ret + theme(axis.text.x=element_text(size=axis_text_size, margin=margin(t=0)))
     ret <- ret + theme(axis.text.y=element_text(size=axis_text_size, margin=margin(r=0)))
     ret <- ret + theme(axis.title=element_text(size=axis_title_size, family=axis_title_family))
@@ -96,14 +96,16 @@
                                                  margin=margin(t=caption_margin),
                                                  family=caption_family, face=caption_face))
     ret <- ret + theme(plot.margin=plot_margin)
-
+    
     ret <- ret + theme(legend.position = "bottom")
-
+    
     ret
-
+    
   }
   theme_set(my_theme)
-
+  
   assign("scale_colour_discrete", function(..., values = my_palette) scale_colour_manual(..., values = values), globalenv())
   assign("scale_fill_discrete", function(..., values = my_palette) scale_fill_manual(..., values = values), globalenv())
+  options(ggplot2.continuous.colour="viridis")
+  options(ggplot2.continuous.fill="viridis")
 }
