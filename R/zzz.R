6ce07fb2bdd726cf241d84c1495d8e06e99c951b
Noelle Pablo <noellebpablo@gmail.com>
2019-05-17 11:04:21 -0500

    fix formatting

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,20 @@
 .onAttach <- function(pkgname, libname) {
-  okabeito_palette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#999999")
+  okabeito_palette <-
+    c(
+      "#E69F00",
+      "#56B4E9",
+      "#009E73",
+      "#F0E442",
+      "#0072B2",
+      "#D55E00",
+      "#CC79A7",
+      "#999999"
+    )
 
-  assign("scale_colour_discrete", function(..., values = okabeito_palette) scale_colour_manual(..., values = values), globalenv())
-  assign("scale_fill_discrete", function(..., values = okabeito_palette) scale_fill_manual(..., values = values), globalenv())
-  options(ggplot2.continuous.colour="viridis")
-  options(ggplot2.continuous.fill="viridis")
+  assign("scale_colour_discrete", function(..., values = okabeito_palette)
+    scale_colour_manual(..., values = values), globalenv())
+  assign("scale_fill_discrete", function(..., values = okabeito_palette)
+    scale_fill_manual(..., values = values), globalenv())
+  options(ggplot2.continuous.colour = "viridis")
+  options(ggplot2.continuous.fill = "viridis")
 }
