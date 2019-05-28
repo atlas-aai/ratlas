bff21624f94744d6ad47fa8833ced0acad60ad77
Noelle Pablo <noellebpablo@gmail.com>
2019-05-28 12:23:29 -0500

    include font startup messages/loading

--- R/zzz.R
+++ R/zzz.R
@@ -17,4 +17,30 @@
     scale_fill_manual(..., values = values), globalenv())
   options(ggplot2.continuous.colour = "viridis")
   options(ggplot2.continuous.fill = "viridis")
+
+  # taken from hrbrthemes/R/zzz.R
+  if (.Platform$OS.type == "windows")  {
+    # nocov start
+    if (interactive())
+      packageStartupMessage("Registering Windows fonts with R")
+    extrafont::loadfonts("win", quiet = TRUE)
+  }
+
+  if (getOption("hrbrthemes.loadfonts", default = FALSE)) {
+    if (interactive())
+      packageStartupMessage("Registering PDF & PostScript fonts with R")
+    extrafont::loadfonts("pdf", quiet = TRUE)
+    extrafont::loadfonts("postscript", quiet = TRUE)
+  }
+
+  fnt <- extrafont::fonttable()
+  if (!any(grepl("Arial[ ]Narrow|Roboto[ ]Condensed", fnt$FamilyName))) {
+    packageStartupMessage("NOTE: Either Arial Narrow or Roboto Condensed fonts
+                          are required to use these themes.")
+    packageStartupMessage("      Please use
+                          hrbrthemes::import_roboto_condensed()
+                          to install Roboto Condensed and")
+    packageStartupMessage("      if Arial Narrow is not on your system,
+                          please see https://bit.ly/arialnarrow")
+  } # nocov end
 }
