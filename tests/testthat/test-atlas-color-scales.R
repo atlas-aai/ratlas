596068884291b5ea02527e2d32a34220dfe1fbcf
Jake Thompson <wjakethompson@gmail.com>
2019-05-31 10:13:34 -0500

    test atlas color scales

---
+++ tests/testthat/test-atlas-color-scales.R
@@ -0,0 +1,31 @@
+context("atlas-colors")
+
+test_that("atlas colors work", {
+  testthat::skip_on_cran()
+  testthat::skip_on_appveyor()
+  testthat::skip_on_travis()
+
+  df <- data.frame(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
+  df2 <- data.frame(x = 1:10, y = 1:10, z = letters[1:10])
+
+  plot <- ggplot(df, aes(x, y, colour = z)) +
+    geom_point() +
+    facet_wrap(~ a) +
+    scale_color_atlas() +
+    theme_atlas()
+
+  plot2 <- ggplot(df, aes(x, y, fill = z)) +
+    geom_col() +
+    facet_wrap(~ a) +
+    scale_fill_atlas(use_black = TRUE) +
+    theme_atlas()
+
+  vdiffr::expect_doppelganger("atlas_color", plot)
+  vdiffr::expect_doppelganger("atlas_fill", plot2)
+  vdiffr::expect_doppelganger("atlas_darken",
+                              plot2 + scale_fill_atlas(darken = 0.2))
+  vdiffr::expect_doppelganger("atlas_lighten",
+                              plot2 + scale_fill_atlas(darken = -0.2))
+  vdiffr::expect_doppelganger("atlas_alpha",
+                              plot2 + scale_fill_atlas(alpha = 0.2))
+})
