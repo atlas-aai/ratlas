bce46a04759e0954ae377c5a435f10dc8a33e5de
Jake Thompson <wjakethompson@gmail.com>
2019-05-30 19:20:33 -0500

    re-export gg_check from hrbrthemes

---
+++ R/utils-spellcheck.R
@@ -0,0 +1,9 @@
+#' Spell check ggplot2 plot labels
+#'
+#' See \code{hrbrthemes::\link[hrbrthemes]{gg_check}} for details.
+#'
+#' @name gg_check
+#' @keywords internal
+#' @export
+#' @importFrom hrbrthemes gg_check
+NULL
