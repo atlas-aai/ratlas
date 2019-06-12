59b147c665ba7d69a9f1ee70c805debed13d6494
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2019-06-12 12:48:04 -0500

    merged skeleton files and skeleton test files

--- tests/testthat/test-skeleton.R
+++ tests/testthat/test-skeleton.R
@@ -1,4 +1,19 @@
-context("tech-report-project-skeletons")
+context("project-skeletons")
+
+test_that("topicguide docx works", {
+  testthat::skip_on_cran()
+
+  # work in a temp directory
+  dir <- tempfile()
+  dir.create(dir)
+  oldwd <- setwd(dir)
+  on.exit(setwd(oldwd), add = TRUE)
+
+  topicguide_skeleton(dir)
+  check_files <- list.files(dir, recursive = TRUE)
+
+  expect_equal(check_files, c("bib/refs.bib", "csl/apa.csl", "index.Rmd"))
+})
 
 test_that("techreport pdf works", {
   testthat::skip_on_cran()
