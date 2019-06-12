75d115cfda66790116ab411b070f909dc18d6528
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2019-06-12 12:53:40 -0500

    merge render and test render files; revert latex file to original format

---
+++ tests/testthat/test-render.R
@@ -0,0 +1,30 @@
+context("rmarkdown renderers")
+
+test_that("topicguide-docx renders", {
+  testthat::skip_on_cran()
+
+  # work in a temp directory
+  dir <- tempfile()
+  dir.create(dir)
+  oldwd <- setwd(dir)
+  on.exit(setwd(oldwd), add = TRUE)
+
+  topicguide_skeleton(dir)
+  rmarkdown::render("index.Rmd")
+  expect_true(file.exists("index.docx"))
+})
+
+
+test_that("techreport-pdf renders", {
+  testthat::skip_on_cran()
+
+  # work in a temp directory
+  dir <- tempfile()
+  dir.create(dir)
+  oldwd <- setwd(dir)
+  on.exit(setwd(oldwd), add = TRUE)
+
+  techreport_skeleton(dir)
+  rmarkdown::render("template.tex")
+  expect_true(file.exists("template.pdf"))
+})
