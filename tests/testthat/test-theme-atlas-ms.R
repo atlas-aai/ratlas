08cbe8133388fda2b2806ee61bcdbf5c71249e8e
Noelle Pablo <34638437+noellepablo@users.noreply.github.com>
2019-06-21 15:41:30 -0500

    Theme atlas ms (#18)
    * first commit; just copied theme-atlas.R code
    
    * change base_family to "Montserrat" and add import_montserrat function
    
    * update_geom_mont_defaults function
    
    * change function names to theme_atlas_ms() and update_geom_font_ms_defaults()
    
    * update function names in demonstration plot code
    
    * add documentation for update_geom_font_ms_defaults()
    
    * update namespace for theme_atlas_ms
    
    * documentation for import_montserrat() and theme_atlas_ms()
    
    * update import_montserrat function to import montserrat from inst/fonts folder
    
    * don't include showtext package anymore, only extrafont
    
    * fix formatting of import_montserrat() code
    
    * user styler::style_file to auto style code
    
    * break up 80+ character lines into two lines
    
    * add expect_doppelganger line to test theme_atlas_ms
    
    * unit tests for theme_atlas_ms()
    
    * unit tests for theme_atlas
    
    * put invisible function calls into test
    
    * add extrafont package to Imports
    
    * update formatting of documentation

---
+++ tests/testthat/test-theme-atlas-ms.R
@@ -0,0 +1,64 @@
+context("theme_atlas_ms")
+thm <- theme_atlas_ms()
+
+test_that("theme_atlas_ms is a theme", {
+  expect_s3_class(thm, "theme")
+})
+
+test_that("theme_atlas_ms uses Montserrat font", {
+  expect_equal(thm$text$family, "Montserrat")
+  expect_equal(thm$plot.title$family, "Montserrat")
+  expect_equal(thm$plot.subtitle$family, "Montserrat")
+  expect_equal(thm$plot.caption$family, "Montserrat")
+  expect_equal(thm$strip.text$family, "Montserrat")
+  expect_equal(thm$axis.title$family, "Montserrat")
+  expect_null(thm$axis.text$family)
+  expect_null(thm$legend.text$family)
+})
+
+test_that("theme_atlas_ms font sizes are correct", {
+  expect_equal(thm$text$size, 11.5)
+  expect_equal(thm$plot.title$size, 18)
+  expect_equal(thm$plot.subtitle$size, 12)
+  expect_equal(thm$strip.text$size, 12)
+  expect_equal(thm$plot.caption$size, 9)
+  expect_equal(thm$axis.title$size, 11.5)
+})
+
+test_that("theme_atlas_ms font colors are correct", {
+  expect_equal(thm$text$colour, "black")
+  expect_equal(thm$axis.text$colour, "grey30")
+})
+
+test_that("theme_atlas_ms grids, axis, and ticks are correct", {
+  expect_equal(invisible(theme_atlas_ms(grid = FALSE)),
+               theme_atlas_ms(grid = FALSE))
+  expect_equal(invisible(theme_atlas_ms(grid = "XY")),
+               theme_atlas_ms(grid = "XY"))
+  expect_equal(invisible(theme_atlas_ms(grid = "xy")),
+               theme_atlas_ms(grid = "xy"))
+
+  expect_equal(invisible(theme_atlas_ms(axis = TRUE)),
+               theme_atlas_ms(axis = TRUE))
+
+  expect_equal(invisible(theme_atlas_ms(axis = FALSE)),
+               theme_atlas_ms(axis = FALSE))
+  expect_equal(invisible(theme_atlas_ms(axis = "xy")),
+               theme_atlas_ms(axis = "xy"))
+  expect_equal(invisible(theme_atlas_ms(axis = "")),
+               theme_atlas_ms(axis = ""))
+  expect_equal(invisible(theme_atlas_ms(ticks = TRUE)),
+               theme_atlas_ms(ticks = TRUE))
+})
+
+test_that("import_montserrat() works", {
+  expect_message(import_montserrat())
+})
+
+test_that("update_geom_font_ms_defaults() works", {
+  expect_equal(update_geom_font_ms_defaults(),
+               update_geom_defaults("text", list(
+                 family = "Montserrat", face = "plain", size = 3.5,
+                 color = "#2b2b2b"
+  )))
+})
