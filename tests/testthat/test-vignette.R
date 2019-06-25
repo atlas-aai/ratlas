875a75efd631e6ceeda9c4873f33a25a7306c4e6
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2019-06-25 16:40:23 -0500

    removed new vignette_skeleton function; addressing issue in test-vignette

--- tests/testthat/test-vignette.R
+++ tests/testthat/test-vignette.R
@@ -9,7 +9,18 @@ test_that("vignette renders", {
   oldwd <- setwd(dir)
   on.exit(setwd(oldwd), add = TRUE)
 
-  vignette_skeleton(dir)
+  resources <- ratlas_file("vignettes")
+  
+  files <- list.files(resources, recursive = TRUE, include.dirs = FALSE)
+  
+  # ensure directories exists
+  new_path <- c(dir, file.path(dir))
+  fs::dir_create(new_path)
+  
+  source <- file.path(resources, files)
+  target <- file.path(dir, files)
+  file.copy(source, target)
+  
   rmarkdown::render("report-writing-vignette.Rmd")
   expect_true(file.exists("report-writing-vignette.html"))
 })
