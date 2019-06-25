61cf56cb738a19d8437af7c4989e814dbdc762ca
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2019-06-25 12:49:47 -0500

    added vignette test file

---
+++ tests/testthat/test-vignette.R
@@ -0,0 +1,15 @@
+context("vignette renders")
+
+test_that("vignette renders", {
+  testthat::skip_on_cran()
+  
+  # work in a temp directory
+  dir <- tempfile()
+  dir.create(dir)
+  oldwd <- setwd(dir)
+  on.exit(setwd(oldwd), add = TRUE)
+
+  rmarkdown::render(here("vignettes/report-writing-vignette.Rmd"), 
+                    output_dir = dir)
+  expect_true(file.exists("report-writing-vignette.html"))
+})
