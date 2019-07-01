3b4138c24c6e93e6abd9b557b3ea40511874093b
Pablo <n013p475@home.ku.edu>
2019-07-01 16:00:24 -0500

    trying to pass lintr, fixing format

--- R/zzz.R
+++ R/zzz.R
@@ -1,21 +1,34 @@
-.onAttach <- function(libname, pkgname) {
-
-  if (.Platform$OS.type == "windows")  {
-    if (interactive()) packageStartupMessage("Registering Windows fonts with R")
+.onLoad <- function(libname, pkgname) {
+  if (.Platform$OS.type == "windows") {
     extrafont::loadfonts("win", quiet = TRUE)
   }
 
   if (getOption("ratlas.loadfonts", default = FALSE)) {
-    if (interactive()) packageStartupMessage("Registering PDF & PostScript fonts with R")
+    extrafont::loadfonts("pdf", quiet = TRUE)
+    extrafont::loadfonts("postscript", quiet = TRUE)
+  }
+}
+
+.onAttach <- function(libname, pkgname) {
+  if (.Platform$OS.type == "windows") {
+    if (interactive()) packageStartupMessage("Registering Windows fonts with R")
+  }
+
+  if (getOption("ratlas.loadfonts", default = FALSE)) {
     extrafont::loadfonts("pdf", quiet = TRUE)
     extrafont::loadfonts("postscript", quiet = TRUE)
   }
 
   fnt <- extrafont::fonttable()
-  if (!any(grepl("Arial[ ]Narrow|Montserrat", fnt$FamilyName))) {
-    packageStartupMessage("NOTE: Either Arial Narrow or Montserrat fonts are required to use these themes.")
-    packageStartupMessage("      Please use ratlas::import_montserrat() to install Montserrat and")
-    packageStartupMessage("      if Arial Narrow is not on your system, please see https://bit.ly/arialnarrow")
+  if (!any(grepl("Arial[ ]Narrow|Montserrat",
+                 fnt[, grep("FamilyName", names(fnt))]))) {
+    packageStartupMessage(
+      "NOTE: Either Arial Narrow or Montserrat
+      fonts are required to use these themes.")
+    packageStartupMessage(
+      "Please use ratlas::import_montserrat() to install Montserrat and")
+    packageStartupMessage(
+      "if Arial Narrow is not on your system,
+      please see https://bit.ly/arialnarrow")
   }
-
 }
