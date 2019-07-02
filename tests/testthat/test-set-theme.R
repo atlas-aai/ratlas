f4c502df09df5c1d19060318afbe1521ce7d3cd1
Pablo <n013p475@home.ku.edu>
2019-07-02 15:44:09 -0500

    add 2nd test case for setting the theme to theme_atlas

--- tests/testthat/test-set-theme.R
+++ tests/testthat/test-set-theme.R
@@ -1,6 +1,6 @@
 context("test-set-theme")
 
-test_that("set_theme correctly sets theme, colors, and fonts", {
+test_that("set_theme correctly sets theme to theme_atlas_ms", {
   invisible(set_theme("theme_atlas_ms"))
   df <- data.frame(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
 
@@ -9,4 +9,15 @@ test_that("set_theme correctly sets theme, colors, and fonts", {
     facet_wrap(~ a)
 
   vdiffr::expect_doppelganger("set_theme_atlas_ms", plot + theme_atlas_ms())
+})
+
+test_that("set_theme correctly sets theme to theme_atlas", {
+  invisible(set_theme())
+  df <- data.frame(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
+
+  plot <- ggplot(df, aes(x, y, colour = z)) +
+    geom_point() +
+    facet_wrap(~ a)
+
+  vdiffr::expect_doppelganger("set_theme_atlas", plot + theme_atlas())
 })
