771544635d9c2331b606f2424ea7ed153cfc0f04
JeffreyCHoover <43454055+JeffreyCHoover@users.noreply.github.com>
2019-07-03 18:02:29 -0500

    Render tech reports (#17)
    * create file for tech report creation
    
    alter render.R to support pdf docs
    
    * code for template changed to pass devtools::check()
    
    * creating file structure for tech_report
    
    * tech report skeleton files
    
    * files for testing skeletons updated
    
    * files for rendering tech report and testing rendered tech report were updated
    
    * code for building latex pdf from template
    
    * update latex code referencing ATLAS png file
    
    * changes made by running styler:::style_active_pkg()
    
    * changes made by re-running devtools::check() and devtools::test()
    
    * fixing a bug in the skeleton.dcf file for the tech report
    
    * revert stylr commit
    
    * merged skeleton files and skeleton test files
    
    * merge render and test render files; revert latex file to original format
    
    * fixing inst/rstudio file structure
    
    * YAML added to index.Rmd
    
    * final changes to pass devtools::check()
    
    * debugging
    
    * test
    
    * rename template variable to reduce confusion
    
    * delete extra project file
    
    * attempt to address travis remotes error
    
    * delete extra project file
    
    * move figures to resource directory
    
    * make mainfont optional
    
    * add extra yaml stuff
    
    * add figures to skeleton test for techreport
    
    * add additional tidyverse packages to suggests
    
    * change from `figure` to `figures`
    
    * trouble shooting travis builds
    
    * update tests
    
    * update tests
    
    * correct Rbuildignore
    
    * remove trouble shooting code
    
    * add back in rendering test for techreport_pdf
    
    * more trouble shooting code to remove
    
    * add verbose to trouble shoot appveyor
    
    * skip pdf rendering on appveyor
    
    * move appveyor skip to correct test

--- R/skeleton.R
+++ R/skeleton.R
@@ -17,3 +17,23 @@ topicguide_skeleton <- function(path) {
 
   TRUE
 }
+
+techreport_skeleton <- function(path) {
+  # copy 'techreport_resources' folder to path
+  resources <- ratlas_file("rstudio", "templates", "project",
+                           "techreport_resources")
+
+  sub_dirs <- list.dirs(resources, recursive = TRUE, full.names = FALSE)
+  sub_dirs <- sub_dirs[-which(sub_dirs == "")]
+  files <- list.files(resources, recursive = TRUE, include.dirs = FALSE)
+
+  # ensure directories exists
+  new_path <- c(path, file.path(path, sub_dirs))
+  fs::dir_create(new_path)
+
+  source <- file.path(resources, files)
+  target <- file.path(path, files)
+  file.copy(source, target)
+
+  TRUE
+}
