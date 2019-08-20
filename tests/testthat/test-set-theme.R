b849199595456175e9fa0f08517240573ec15200
Jake Thompson <wjakethompson@gmail.com>
2019-08-20 13:51:40 -0500

    updates tests for theme setting

--- tests/testthat/test-set-theme.R
+++ tests/testthat/test-set-theme.R
@@ -1,22 +1,24 @@
 context("test-set-theme")
 
 test_that("set_theme correctly sets theme to theme_atlas_ms", {
-  invisible(set_theme("theme_atlas_ms"))
+  set_theme("Montserrat")
   df <- data.frame(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
 
   plot <- ggplot(df, aes(x, y, colour = z)) +
     geom_point() +
+    geom_label(aes(label = z)) +
     facet_wrap(~ a)
 
   vdiffr::expect_doppelganger("set_theme_atlas_ms", plot + theme_atlas_ms())
 })
 
 test_that("set_theme correctly sets theme to theme_atlas", {
-  invisible(set_theme())
+  set_theme("Arial Narrow")
   df <- data.frame(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
 
   plot <- ggplot(df, aes(x, y, colour = z)) +
     geom_point() +
+    geom_label(aes(label = z)) +
     facet_wrap(~ a)
 
   vdiffr::expect_doppelganger("set_theme_atlas", plot + theme_atlas())
