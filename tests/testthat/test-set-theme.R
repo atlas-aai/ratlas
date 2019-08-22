18f00987de22e73dce379983fadccd30c95ab7cd
Jake Thompson <wjakethompson@gmail.com>
2019-08-22 16:47:08 -0500

    trying to figure out tests

--- tests/testthat/test-set-theme.R
+++ tests/testthat/test-set-theme.R
@@ -1,19 +1,8 @@
 context("test-set-theme")
 
-test_that("set_theme correctly sets theme to theme_atlas_ms", {
-  set_theme("Montserrat")
-  df <- data.frame(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
-
-  plot <- ggplot(df, aes(x, y, colour = z)) +
-    geom_point() +
-    geom_label(aes(label = z)) +
-    facet_wrap(~ a)
-
-  vdiffr::expect_doppelganger("set_theme_atlas_ms", plot + theme_atlas_ms())
-})
-
+# Test theme fonts -------------------------------------------------------------
 test_that("set_theme correctly sets theme to theme_atlas", {
-  set_theme("Arial Narrow")
+  set_theme(font = "Arial Narrow")
   df <- data.frame(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
 
   plot <- ggplot(df, aes(x, y, colour = z)) +
@@ -21,5 +10,5 @@ test_that("set_theme correctly sets theme to theme_atlas", {
     geom_label(aes(label = z)) +
     facet_wrap(~ a)
 
-  vdiffr::expect_doppelganger("set_theme_atlas", plot + theme_atlas())
+  vdiffr::expect_doppelganger("set_theme_atlas", plot)
 })
