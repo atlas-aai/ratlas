3a75439e11afcd8e8f8bf0ee812c0e3363b1f284
Jake Thompson <wjakethompson@gmail.com>
2019-08-28 09:37:17 -0500

    updates to set theme

--- R/set-theme.R
+++ R/set-theme.R
@@ -13,21 +13,35 @@
 #' set_theme("Montserrat")
 #'
 #' @export
-set_theme <- function(font = "Arial Narrow"){
-  pos <- 1
-  envir <- as.environment(pos)
-  assign("scale_colour_discrete", function(..., values = palette_OkabeIto)
-    scale_colour_manual(..., values = values), envir = envir)
-  assign("scale_fill_discrete", function(..., values = palette_OkabeIto)
-    scale_fill_manual(..., values = values), envir = envir)
-  options(ggplot2.continuous.colour = "viridis")
-  options(ggplot2.continuous.fill = "viridis")
+set_theme <- function(font = "Arial Narrow", discrete = "okabeito",
+                      continuous = "viridis") {
+  cont_option <- switch(continuous,
+                        magma = "A",
+                        inferno = "B",
+                        plasma = "C",
+                        viridis = "D",
+                        cividis = "E")
+  disc_option <- switch(discrete,
+                        okabeito = palette_OkabeIto,
+                        atlas = palette_atlas)
 
   if (font == "Arial Narrow") {
-    theme_set(theme_atlas())
+    ggplot2::theme_set(theme_atlas())
     update_geom_font_defaults()
   } else if (font == "Montserrat") {
-    theme_set(theme_atlas_ms())
+    ggplot2::theme_set(theme_atlas_ms())
     update_geom_font_ms_defaults()
+  }
+
+  if (!is.null(disc_option)) {
+    assign("scale_colour_discrete", function(..., values = disc_option)
+      scale_colour_manual(..., values = values), pos = ".GlobalEnv")
+    assign("scale_fill_discrete", function(..., values = disc_option)
+      scale_fill_manual(..., values = values), pos = ".GlobalEnv")
+  }
+  if (!is.null(cont_option)) {
+    assign("scale_fill_continuous", function(..., option = cont_option)
+      scale_fill_continuous(..., option = option, type = "viridis"),
+      pos = ".GlobalEnv")
   }
 }
--- tests/testthat/test-set-theme.R
+++ tests/testthat/test-set-theme.R
@@ -1,14 +1,110 @@
 context("test-set-theme")
 
-# Test theme fonts -------------------------------------------------------------
-test_that("set_theme correctly sets theme to theme_atlas", {
-  set_theme(font = "Arial Narrow")
-  df <- data.frame(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
+# Arial Narrow -----------------------------------------------------------------
+set_theme(font = "Arial Narrow", discrete = "okabeito", continuous = "viridis")
+thm <- theme_get()
 
-  plot <- ggplot(df, aes(x, y, colour = z)) +
-    geom_point() +
-    geom_label(aes(label = z)) +
-    facet_wrap(~ a)
+test_that("theme_atlas is a theme", {
+  expect_s3_class(thm, "theme")
+})
 
-  vdiffr::expect_doppelganger("set_theme_atlas", plot)
+test_that("theme_atlas uses Arial Narrow font", {
+  expect_equal(thm$text$family, "Arial Narrow")
+  expect_equal(thm$plot.title$family, "Arial Narrow")
+  expect_equal(thm$plot.subtitle$family, "Arial Narrow")
+  expect_equal(thm$plot.caption$family, "Arial Narrow")
+  expect_equal(thm$strip.text$family, "Arial Narrow")
+  expect_equal(thm$axis.title$family, "Arial Narrow")
+  expect_null(thm$axis.text$family)
+  expect_null(thm$legend.text$family)
+})
+
+test_that("theme_atlas font sizes are correct", {
+  expect_equal(thm$text$size, 11.5)
+  expect_equal(thm$plot.title$size, 18)
+  expect_equal(thm$plot.subtitle$size, 12)
+  expect_equal(thm$strip.text$size, 12)
+  expect_equal(thm$plot.caption$size, 9)
+  expect_equal(thm$axis.title$size, 11.5)
+})
+
+test_that("theme_atlas font colors are correct", {
+  expect_equal(thm$text$colour, "black")
+  expect_equal(thm$axis.text$colour, "grey30")
+})
+
+test_that("theme_atlas grids, axis, and ticks are correct", {
+  expect_equal(invisible(theme_atlas(grid = FALSE)),
+               theme_atlas(grid = FALSE))
+  expect_equal(invisible(theme_atlas(grid = "XY")),
+               theme_atlas(grid = "XY"))
+  expect_equal(invisible(theme_atlas(grid = "xy")),
+               theme_atlas(grid = "xy"))
+
+  expect_equal(invisible(theme_atlas(axis = TRUE)),
+               theme_atlas(axis = TRUE))
+
+  expect_equal(invisible(theme_atlas(axis = FALSE)),
+               theme_atlas(axis = FALSE))
+  expect_equal(invisible(theme_atlas(axis = "xy")),
+               theme_atlas(axis = "xy"))
+  expect_equal(invisible(theme_atlas(axis = "")),
+               theme_atlas(axis = ""))
+  expect_equal(invisible(theme_atlas(ticks = TRUE)),
+               theme_atlas(ticks = TRUE))
+})
+
+
+# Montserrat -------------------------------------------------------------------
+set_theme(font = "Montserrat", discrete = "okabeito", continuous = "viridis")
+thm <- theme_get()
+
+test_that("theme_atlas_ms is a theme", {
+  expect_s3_class(thm, "theme")
+})
+
+test_that("theme_atlas_ms uses Montserrat font", {
+  expect_equal(thm$text$family, "Montserrat")
+  expect_equal(thm$plot.title$family, "Montserrat")
+  expect_equal(thm$plot.subtitle$family, "Montserrat")
+  expect_equal(thm$plot.caption$family, "Montserrat")
+  expect_equal(thm$strip.text$family, "Montserrat")
+  expect_equal(thm$axis.title$family, "Montserrat")
+  expect_null(thm$axis.text$family)
+  expect_null(thm$legend.text$family)
+})
+
+test_that("theme_atlas_ms font sizes are correct", {
+  expect_equal(thm$text$size, 11.5)
+  expect_equal(thm$plot.title$size, 18)
+  expect_equal(thm$plot.subtitle$size, 12)
+  expect_equal(thm$strip.text$size, 12)
+  expect_equal(thm$plot.caption$size, 9)
+  expect_equal(thm$axis.title$size, 11.5)
+})
+
+test_that("theme_atlas_ms font colors are correct", {
+  expect_equal(thm$text$colour, "black")
+  expect_equal(thm$axis.text$colour, "grey30")
+})
+
+test_that("theme_atlas_ms grids, axis, and ticks are correct", {
+  expect_equal(invisible(theme_atlas_ms(grid = FALSE)),
+               theme_atlas_ms(grid = FALSE))
+  expect_equal(invisible(theme_atlas_ms(grid = "XY")),
+               theme_atlas_ms(grid = "XY"))
+  expect_equal(invisible(theme_atlas_ms(grid = "xy")),
+               theme_atlas_ms(grid = "xy"))
+
+  expect_equal(invisible(theme_atlas_ms(axis = TRUE)),
+               theme_atlas_ms(axis = TRUE))
+
+  expect_equal(invisible(theme_atlas_ms(axis = FALSE)),
+               theme_atlas_ms(axis = FALSE))
+  expect_equal(invisible(theme_atlas_ms(axis = "xy")),
+               theme_atlas_ms(axis = "xy"))
+  expect_equal(invisible(theme_atlas_ms(axis = "")),
+               theme_atlas_ms(axis = ""))
+  expect_equal(invisible(theme_atlas_ms(ticks = TRUE)),
+               theme_atlas_ms(ticks = TRUE))
 })
