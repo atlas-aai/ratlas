faef7ccefe9f46b87e620fde852b3fbd05622755
Jake Thompson <wjakethompson@gmail.com>
2019-10-06 12:34:39 -0500

    fix lints

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,4 @@
 # nocov start
-.onLoad <- function(libname, pkgname) {
-  pdfFonts <- grDevices::pdfFonts()
-  postscriptFonts <- grDevices::postscriptFonts()
-  if (.Platform$OS.type == "windows") {
-    windowsFonts <- grDevices::windowsFonts()
-  }
-}
-
 .onAttach <- function(libname, pkgname) {
   if (.Platform$OS.type == "windows") {
     if (interactive()) {
@@ -16,18 +8,20 @@
   }
 
   if (getOption("ratlas.loadfonts", default = FALSE)) {
-    if (interactive()){
+    if (interactive()) {
       packageStartupMessage("Registering PDF & PostScript fonts with R")
     }
     extrafont::loadfonts("pdf", quiet = TRUE)
     extrafont::loadfonts("postscript", quiet = TRUE)
   }
 
+  # nolint start
   fnt <- extrafont::fonttable()
   if (!any(grepl("Arial[ ]Narrow|Montserrat", fnt$FamilyName))) {
     packageStartupMessage("NOTE: Either Arial Narrow or Montserrat fonts are required to use these themes.")
     packageStartupMessage("      Please use ratlas::import_montserrat() to install Montserrat and")
     packageStartupMessage("      if Arial Narrow is not on your system, please see https://bit.ly/arialnarrow")
   }
+  # nolint end
 }
 # nocov end
