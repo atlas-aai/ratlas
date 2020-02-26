0112460ab8b9f24bc75c6a2314ca39cdda5e490d
Noelle Pablo <34638437+noellepablo@users.noreply.github.com>
2020-02-26 09:33:08 -0800

    Xaringan template (#27)
    * add xaringan package to "imports"
    
    * create directory containing atlas presentation template files
    
    * change version of xaringan package
    
    * change font color of slide counter on header slides
    
    * minor tweaks to skeleton rmd
    
    * some reorganizing and minor tweaks to css
    
    Trying to more closely match the ppt template
    
    * moving some css to the other css file
    
    * suggesting fontawesome for xaringan final slide
    
    * new libs part of default xaringan template
    
    * removing fontawesome becuase it's not on cran. LAME.
    
    * replace imgbb urls with imgur urls, add "reg-slide" class for other atlas background
    
    * remove libs from package
    
    * move xaringan css into resources
    
    * add wrapper  function for xaringan slides
    
    * update template for atlas xaringan slides
    
    * update documentation
    
    * ignore lints in chunk options
    
    * finalize organization
    
    * fix  typo
    
    * try to fix tests
    
    * fix xaringan test
    
    * add bold weight
    
    * tweaks to font display
    
    Co-authored-by: Jake Thompson <wjakethompson@users.noreply.github.com>

--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -19,7 +19,7 @@ test_that("topicguide-docx renders", {
   on.exit(setwd(oldwd), add = TRUE)
 
   topicguide_skeleton(dir)
-  rmarkdown::render("index.Rmd")
+  rmarkdown::render("index.Rmd", quiet = TRUE)
   expect_true(file.exists("index.docx"))
 })
 
@@ -35,6 +35,32 @@ test_that("techreport-pdf renders", {
   on.exit(setwd(oldwd), add = TRUE)
 
   techreport_skeleton(dir)
-  suppressWarnings(rmarkdown::render("index.Rmd"))
+  suppressWarnings(rmarkdown::render("index.Rmd", quiet = TRUE))
   expect_true(file.exists("index.pdf"))
+})
+
+test_that("slides-html renders", {
+  testthat::skip_on_cran()
+  testthat::skip_on_travis()
+  testthat::skip_on_appveyor()
+
+  resources <- here::here("inst", "rmarkdown", "templates",
+                          "atlas-presentation", "skeleton")
+
+  sub_dirs <- list.dirs(resources, recursive = TRUE, full.names = FALSE)
+  sub_dirs <- sub_dirs[-which(sub_dirs == "")]
+  files <- list.files(resources, recursive = TRUE, include.dirs = FALSE)
+
+  # work in a temp directory
+  dir <- tempdir()
+  new_dir <- c(dir, file.path(dir, sub_dirs))
+  fs::dir_create(new_dir)
+  oldwd <- setwd(dir)
+  on.exit(setwd(oldwd), add = TRUE)
+
+  source <- file.path(resources, files)
+  target <- file.path(dir, files)
+  file.copy(source, target)
+  suppressWarnings(rmarkdown::render("skeleton.Rmd", quiet = TRUE))
+  expect_true(file.exists("skeleton.html"))
 })
