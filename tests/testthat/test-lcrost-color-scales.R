a4e06f3ba923b7011eecb619ff967e7516718b2b
Jake Thompson <wjakethompson@gmail.com>
2020-06-25 13:44:37 -0500

    add tests for lcrost scales

---
+++ tests/testthat/test-lcrost-color-scales.R
@@ -0,0 +1,38 @@
+context("test-lcrost-color-scales")
+library(ggplot2)
+
+test_that("Lisa Charlotte Rost colors work", {
+  testthat::skip_on_cran()
+
+  df <- data.frame(x = 1:3, y = 1:3, z = c("a", "b", "a"), a = 1)
+  df2 <- data.frame(x = 1:10, y = 1:10, z = letters[1:10], a = 1)
+
+  plot <- ggplot(df, aes(x, y, colour = z)) +
+    geom_point() +
+    facet_wrap(~ a) +
+    scale_color_lcrost() +
+    theme_atlas()
+
+  plot2 <- ggplot(df, aes(x, y, fill = z)) +
+    geom_col() +
+    facet_wrap(~ a) +
+    scale_fill_lcrost(use_black = TRUE) +
+    theme_atlas()
+
+  plot3 <- ggplot(df2, aes(x, y, colour = z)) +
+    geom_point() +
+    facet_wrap(~ a) +
+    scale_color_lcrost() +
+    theme_atlas()
+
+  expect_warning(ggplot2::ggplot_build(plot3), "Insufficient values")
+  expect_warning(ggplot2::ggplot_build(plot3), "only 8 provided")
+  vdiffr::expect_doppelganger("lcrost_color", plot)
+  vdiffr::expect_doppelganger("lcrost_fill", plot2)
+  vdiffr::expect_doppelganger("lcrost_darken",
+                              plot2 + scale_fill_lcrost(darken = 0.2))
+  vdiffr::expect_doppelganger("lcrost_lighten",
+                              plot2 + scale_fill_lcrost(darken = -0.2))
+  vdiffr::expect_doppelganger("lcrost_alpha",
+                              plot2 + scale_fill_lcrost(alpha = 0.2))
+})
