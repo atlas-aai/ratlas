3c9a04823a567969c820f33cc5a9b827c6db482c
Jake Thompson <wjakethompson@gmail.com>
2020-06-25 13:40:13 -0500

    add lcrost palette

--- R/palettes.R
+++ R/palettes.R
@@ -25,3 +25,16 @@ palette_atlas <- c("#9BD3DD", "#F2A900", "#8FD6BD", "#F0EC74", "#165C7D",
 #' @export
 palette_atlas_black <- c("#9BD3DD", "#F2A900", "#8FD6BD", "#F0EC74", "#165C7D",
                          "#000000")
+
+#' Color palette proposed by Lisa Charlotte Rost
+#'
+#' A colorblind friendly palette taken from the article [What to consider when
+#' visualizing data for colorblind readers](https://blog.datawrapper.de/colorblindness-part2/)
+#' @export
+palette_lcrost <- c("#193CBC", "#1473AF", "#589ACF", "#89C3EF", "#EA594E",
+                    "#E5B039", "#EDE65A", "#999999")
+
+#' @rdname palette_lcrost
+#' @export
+palette_lcrost_black <- c("#193CBC", "#1473AF", "#589ACF", "#89C3EF", "#EA594E",
+                          "#E5B039", "#EDE65A", "#000000")
