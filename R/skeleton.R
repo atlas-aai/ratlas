5392a6e3b0ed5310c492c1b322af340e0fa61aab
Jake Thompson <wjakethompson@gmail.com>
2020-07-17 15:54:39 -0500

    add html skeleton

--- R/skeleton.R
+++ R/skeleton.R
@@ -71,7 +71,7 @@ topicguide_pdf_skeleton <- function(path) {
 }
 
 techreport_pdf_skeleton <- function(path) {
-  # copy 'techreport_resources' folder to path
+  # copy 'techreport_pdf_resources' folder to path
   resources <- ratlas_file("rstudio", "templates", "project",
                            "techreport_pdf_resources")
 
@@ -102,6 +102,38 @@ techreport_pdf_skeleton <- function(path) {
   # rename index.Rmd
   fs::file_move(file.path(path, "index.Rmd"),
                 file.path(path, paste0(tolower(basename(path)), ".Rmd")))
+
+  TRUE
+}
+
+techreport_html_skeleton <- function(path) {
+  # copy 'techreport_html_resources' folder to path
+  resources <- ratlas_file("rstudio", "templates", "project",
+                           "techreport_html_resources")
+
+  sub_dirs <- list.dirs(resources, recursive = TRUE, full.names = FALSE)
+  sub_dirs <- sub_dirs[-which(sub_dirs == "")]
+  files <- list.files(resources, recursive = TRUE, include.dirs = FALSE)
+
+  # ensure directories exists
+  new_path <- c(path, file.path(path, sub_dirs))
+  fs::dir_create(new_path)
+
+  source <- file.path(resources, files)
+  target <- file.path(path, files)
+  file.copy(source, target)
+
+  # add book_filename to _bookdown.yml and default to the base path name
+  f <- file.path(path, "_bookdown.yml")
+  x <- xfun::read_utf8(f)
+  xfun::write_utf8(c(sprintf('book_filename: "%s"',
+                             stringr::str_to_title(basename(path))),
+                     x,
+                     "",
+                     "rmd_files: [",
+                     sprintf('  "index.Rmd"', tolower(basename(path))),
+                     "]"),
+                   f)
 
   TRUE
 }
