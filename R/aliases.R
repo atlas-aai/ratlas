a28dc15204934ed19980b5fcd28317b7e1b51992
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2021-08-20 14:48:51 -0400

    adding factor levels for percentage values

--- R/aliases.R
+++ R/aliases.R
@@ -10,6 +10,9 @@
 #' `dlm_complexity` contains all complexity band names for ELA, mathematics,
 #' science, writing, and communication.
 #'
+#' `factor_pct` contains ordered percentage values rounded to whole numbers.
+#' The major contribution is that this object differentiates between 0 and <1.
+#'
 #' @name dlmalias
 
 #' @rdname dlmalias
@@ -25,3 +28,7 @@ dlm_grades <- c(3:5, "3-5", 6:8, "6-8", 9:12, "9-10", "11-12", "9-12",
 #' @rdname dlmalias
 #' @export
 dlm_complexity <- c("Foundational", "Band 1", "Band 2", "Band 3")
+
+#' @rdname dlmalias
+#' @export
+factor_pct <- c("0", "<1", as.character(1:100))
