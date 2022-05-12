9a7ba3854f2e1298fd6a62eb65facb4934f4c70b
Jake Thompson <wjakethompson@gmail.com>
2022-05-12 10:40:50 -0500

    add measr pdf format

--- R/render.R
+++ R/render.R
@@ -146,6 +146,53 @@ topicguide_pdf <- function(...) {
 }
 
 
+#' Create an R Markdown PDF measr Report
+#'
+#' This is a function called in the output of the yaml of the Rmd file to
+#' specify using the standard measr report document formatting.
+#'
+#' @param ... Arguments to be passed to `[bookdown::pdf_document2]`
+#'
+#' @return A modified `pdf_document2` with the standard tech report formatting.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   output: ratlas::measr_pdf
+#' }
+measr_pdf <- function(...) {
+  report_template <- find_resource("measr_pdf", "template.tex")
+  base <- bookdown::pdf_document2(template = report_template,
+                                  latex_engine = "xelatex",
+                                  citation_package = "biblatex",
+                                  keep_tex = TRUE, number_sections = FALSE,
+                                  highlight = NULL, ...)
+
+  # nolint start
+  base$knitr$opts_chunk$comment <- "#>"
+  base$knitr$opts_chunk$message <- FALSE
+  base$knitr$opts_chunk$warning <- FALSE
+  base$knitr$opts_chunk$error <- FALSE
+  base$knitr$opts_chunk$echo <- TRUE
+  base$knitr$opts_chunk$cache <- FALSE
+  base$knitr$opts_chunk$fig.width <- 7
+  base$knitr$opts_chunk$fig.asp <- 0.618
+  base$knitr$opts_chunk$fig.ext <- "pdf"
+  base$knitr$opts_chunk$fig.align <- "center"
+  base$knitr$opts_chunk$fig.retina <- 3
+  base$knitr$opts_chunk$fig.path <- "figures/"
+  base$knitr$opts_chunk$fig.pos <- "H"
+  base$knitr$opts_chunk$out.extra <- ""
+  base$knitr$opts_chunk$out.width <- "100%"
+  base$knitr$opts_chunk$fig.show <- "hold"
+  # nolint end
+
+  base$knitr$knit_hooks$plot <- hook_tex_plot_rat
+
+  base
+}
+
+
 #' Create an R Markdown PDF Document Tech Report
 #'
 #' This is a function called in the output of the yaml of the Rmd file to
--- R/skeleton.R
+++ R/skeleton.R
@@ -70,6 +70,30 @@ topicguide_pdf_skeleton <- function(path) {
   TRUE
 }
 
+measr_pdf_skeleton <- function(path) {
+  # copy 'topicguide_pdf_resources' folder to path
+  resources <- ratlas_file("rstudio", "templates", "project",
+                           "measr_pdf_resources")
+
+  sub_dirs <- list.dirs(resources, recursive = TRUE, full.names = FALSE)
+  sub_dirs <- sub_dirs[-which(sub_dirs == "")]
+  files <- list.files(resources, recursive = TRUE, include.dirs = FALSE)
+
+  # ensure directories exists
+  new_path <- c(path, file.path(path, sub_dirs))
+  fs::dir_create(new_path)
+
+  source <- file.path(resources, files)
+  target <- file.path(path, files)
+  file.copy(source, target)
+
+  # rename index.Rmd
+  fs::file_move(file.path(path, "index.Rmd"),
+                file.path(path, paste0(tolower(basename(path)), ".Rmd")))
+
+  TRUE
+}
+
 techreport_pdf_skeleton <- function(path) {
   # copy 'techreport_pdf_resources' folder to path
   resources <- ratlas_file("rstudio", "templates", "project",
