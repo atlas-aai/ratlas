c7023a628b043d9b92d7543db5df5773945c683b
Jake Thompson <wjakethompson@gmail.com>
2025-07-02 13:05:54 -0500

    remove atlas color palette

--- R/palettes.R
+++ R/palettes.R
@@ -13,19 +13,6 @@ palette_okabeito <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
 palette_okabeito_black <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442",
                             "#0072B2", "#D55E00", "#CC79A7", "#000000")
 
-#' Official color palette for ATLAS
-#'
-#' Official brand colors for Accessible Teaching, Learning, and Assessment
-#' Systems.
-#' @export
-palette_atlas <- c("#9BD3DD", "#F2A900", "#8FD6BD", "#F0EC74", "#165C7D",
-                   "#999999")
-
-#' @rdname palette_atlas
-#' @export
-palette_atlas_black <- c("#9BD3DD", "#F2A900", "#8FD6BD", "#F0EC74", "#165C7D",
-                         "#000000")
-
 #' Color palette proposed by Lisa Charlotte Rost
 #'
 #' A colorblind friendly palette taken from the article [What to consider when
