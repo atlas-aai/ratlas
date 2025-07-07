b4fed0f9c8a09a4f8d6b4c49b3545223c70f1e19
Jake Thompson <wjakethompson@gmail.com>
2025-07-07 08:36:09 -0500

    streamline report skeletons

--- R/skeleton.R
+++ R/skeleton.R
@@ -1,7 +1,13 @@
-topicguide_docx_skeleton <- function(path) {
-  # copy 'topicguide_docx_resources' folder to path
+project_skeleton <- function(path, type) {
+  type <- rlang::arg_match(
+    type,
+    c("techreport_html", "techreport_pdf", "topicguide_docx", "topicguide_pdf",
+      "measr_pdf")
+  )
+
+  # copy 'techreport_html_resources' folder to path
   resources <- ratlas_file("rstudio", "templates", "project",
-                           "topicguide_docx_resources")
+                           paste0(type, "_resources"))
 
   sub_dirs <- list.dirs(resources, recursive = TRUE, full.names = FALSE)
   sub_dirs <- sub_dirs[-which(sub_dirs == "")]
@@ -16,112 +22,18 @@ topicguide_docx_skeleton <- function(path) {
   file.copy(source, target)
 
   # add book_filename to _bookdown.yml and default to the base path name
-  f <- file.path(path, "_bookdown.yml")
-  x <- xfun::read_utf8(f)
-  xfun::write_utf8(c(sprintf('book_filename: "%s"',
-                             stringr::str_to_title(basename(path))),
-                     x,
-                     "",
-                     "rmd_files: [",
-                     sprintf('  "%s.Rmd"', tolower(basename(path))),
-                     "]"),
-                   f)
-
-  # rename index.Rmd
-  fs::file_move(file.path(path, "index.Rmd"),
-                file.path(path, paste0(tolower(basename(path)), ".Rmd")))
-
-  TRUE
-}
-
-topicguide_pdf_skeleton <- function(path) {
-  # copy 'topicguide_pdf_resources' folder to path
-  resources <- ratlas_file("rstudio", "templates", "project",
-                           "topicguide_pdf_resources")
-
-  sub_dirs <- list.dirs(resources, recursive = TRUE, full.names = FALSE)
-  sub_dirs <- sub_dirs[-which(sub_dirs == "")]
-  files <- list.files(resources, recursive = TRUE, include.dirs = FALSE)
-
-  # ensure directories exists
-  new_path <- c(path, file.path(path, sub_dirs))
-  fs::dir_create(new_path)
-
-  source <- file.path(resources, files)
-  target <- file.path(path, files)
-  file.copy(source, target)
-
-  # add book_filename to _bookdown.yml and default to the base path name
-  f <- file.path(path, "_bookdown.yml")
-  x <- xfun::read_utf8(f)
-  xfun::write_utf8(c(sprintf('book_filename: "%s"',
-                             stringr::str_to_title(basename(path))),
-                     x,
-                     "",
-                     "rmd_files: [",
-                     sprintf('  "%s.Rmd"', tolower(basename(path))),
-                     "]"),
-                   f)
-
-  # rename index.Rmd
-  fs::file_move(file.path(path, "index.Rmd"),
-                file.path(path, paste0(tolower(basename(path)), ".Rmd")))
-
-  TRUE
-}
-
-measr_pdf_skeleton <- function(path) {
-  # copy 'topicguide_pdf_resources' folder to path
-  resources <- ratlas_file("rstudio", "templates", "project",
-                           "measr_pdf_resources")
-
-  sub_dirs <- list.dirs(resources, recursive = TRUE, full.names = FALSE)
-  sub_dirs <- sub_dirs[-which(sub_dirs == "")]
-  files <- list.files(resources, recursive = TRUE, include.dirs = FALSE)
-
-  # ensure directories exists
-  new_path <- c(path, file.path(path, sub_dirs))
-  fs::dir_create(new_path)
-
-  source <- file.path(resources, files)
-  target <- file.path(path, files)
-  file.copy(source, target)
-
-  # rename index.Rmd
-  fs::file_move(file.path(path, "index.Rmd"),
-                file.path(path, paste0(tolower(basename(path)), ".Rmd")))
-
-  TRUE
-}
-
-techreport_pdf_skeleton <- function(path) {
-  # copy 'techreport_pdf_resources' folder to path
-  resources <- ratlas_file("rstudio", "templates", "project",
-                           "techreport_pdf_resources")
-
-  sub_dirs <- list.dirs(resources, recursive = TRUE, full.names = FALSE)
-  sub_dirs <- sub_dirs[-which(sub_dirs == "")]
-  files <- list.files(resources, recursive = TRUE, include.dirs = FALSE)
-
-  # ensure directories exists
-  new_path <- c(path, file.path(path, sub_dirs))
-  fs::dir_create(new_path)
-
-  source <- file.path(resources, files)
-  target <- file.path(path, files)
-  file.copy(source, target)
-
-  # add book_filename to _bookdown.yml and default to the base path name
-  f <- file.path(path, "_bookdown.yml")
-  x <- xfun::read_utf8(f)
-  xfun::write_utf8(c(sprintf('book_filename: "%s"',
-                             stringr::str_to_title(basename(path))),
-                     x,
-                     "",
-                     "rmd_files: [",
-                     sprintf('  "%s.Rmd"', tolower(basename(path))),
-                     "]"),
-                   f)
+  if (file.exists(file.path(path, "_bookdown.yml"))) {
+    f <- file.path(path, "_bookdown.yml")
+    x <- xfun::read_utf8(f)
+    xfun::write_utf8(c(sprintf('book_filename: "%s"',
+                               stringr::str_to_title(basename(path))),
+                       x,
+                       "",
+                       "rmd_files: [",
+                       sprintf('  "%s.Rmd"', tolower(basename(path))),
+                       "]"),
+                     f)
+  }
 
   # rename index.Rmd
   fs::file_move(file.path(path, "index.Rmd"),
@@ -131,33 +43,21 @@ techreport_pdf_skeleton <- function(path) {
 }
 
 techreport_html_skeleton <- function(path) {
-  # copy 'techreport_html_resources' folder to path
-  resources <- ratlas_file("rstudio", "templates", "project",
-                           "techreport_html_resources")
+  project_skeleton(path, type = "techreport_html")
+}
 
-  sub_dirs <- list.dirs(resources, recursive = TRUE, full.names = FALSE)
-  sub_dirs <- sub_dirs[-which(sub_dirs == "")]
-  files <- list.files(resources, recursive = TRUE, include.dirs = FALSE)
+techreport_pdf_skeleton <- function(path) {
+  project_skeleton(path, type = "techreport_pdf")
+}
 
-  # ensure directories exists
-  new_path <- c(path, file.path(path, sub_dirs))
-  fs::dir_create(new_path)
+topicguide_docx_skeleton <- function(path) {
+  project_skeleton(path, type = "topicguide_docx")
+}
 
-  source <- file.path(resources, files)
-  target <- file.path(path, files)
-  file.copy(source, target)
+topicguide_pdf_skeleton <- function(path) {
+  project_skeleton(path, type = "topicguide_pdf")
+}
 
-  # add book_filename to _bookdown.yml and default to the base path name
-  f <- file.path(path, "_bookdown.yml")
-  x <- xfun::read_utf8(f)
-  xfun::write_utf8(c(sprintf('book_filename: "%s"',
-                             stringr::str_to_title(basename(path))),
-                     x,
-                     "",
-                     "rmd_files: [",
-                     '  "index.Rmd"',
-                     "]"),
-                   f)
-
-  TRUE
+measr_pdf_skeleton <- function(path) {
+  project_skeleton(path, type = "measr_pdf")
 }
