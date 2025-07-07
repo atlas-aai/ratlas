042ada33080a4612509cb0a7feb05901805b5e04
Jake Thompson <wjakethompson@gmail.com>
2025-07-07 09:27:06 -0500

    add performance levels

--- R/aliases-dlm.R
+++ R/aliases-dlm.R
@@ -10,6 +10,11 @@
 #' `dlm_complexity` contains all complexity band names for ELA, mathematics,
 #' science, writing, and communication.
 #'
+#' `dlm_performance` and `dlm_performance_full` contain the performance level
+#' names for ELA, mathematics and science. `dlm_performance` uses the common
+#' names (e.g., "Approaching"), whereas `dlm_performance_full` uses the official
+#' names (e.g., "Approaching the Target").
+#'
 #' @name dlmalias
 
 #' @rdname dlmalias
@@ -25,3 +30,12 @@ dlm_grades <- c(3:5, "3-5", 6:8, "6-8", 9:12, "9-10", "11-12", "9-12",
 #' @rdname dlmalias
 #' @export
 dlm_complexity <- c("Foundational", "Band 1", "Band 2", "Band 3")
+
+#' @rdname dlmalias
+#' @export
+dlm_performance <- c("Emerging", "Approaching", "Target", "Advanced")
+
+#' @rdname dlmalias
+#' @export
+dlm_performance_full <- c("Emerging", "Approaching the Target", "At Target",
+                          "Advanced")
