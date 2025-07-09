73a4943b6b7d3f33b0f8a82deb5e217e48d4ee74
Jake Thompson <wjakethompson@gmail.com>
2025-07-09 14:12:29 -0500

    update data

--- data-raw/dlm-info.R
+++ data-raw/dlm-info.R
@@ -1,3 +1,24 @@
+dlm_ll <- c("Initial Precursor", "Initial", "Distal Precursor", "Precursor",
+            "Proximal Precursor", "Target", "Successor")
+
+dlm_grades <- c(3:5, "3-5", 6:8, "6-8", 9:12, "9-10", "11-12", "9-12",
+                "Biology")
+
+dlm_complexity <- c("Foundational", "Band 1", "Band 2", "Band 3")
+
+dlm_performance <- c("Emerging", "Approaching", "Target", "Advanced")
+
+dlm_performance_full <- c("Emerging", "Approaching the Target", "At Target",
+                          "Advanced")
+
+dlm_labels <- list(
+  linkage_levels = dlm_ll,
+  grades = dlm_grades,
+  complexity_bands = dlm_complexity,
+  performance_levels = dlm_performance,
+  performance_levels_full = dlm_performance_full
+)
+
 dlm_ll_info <- tibble::tribble(
   ~subject,    ~linkage_level,                ~name, ~value,
   "ELA",  "Initial Precursor",  "Initial Precursor",     1L,
@@ -15,4 +36,4 @@ dlm_ll_info <- tibble::tribble(
   "Sci",             "Target",             "Target",     3L
 )
 
-usethis::use_data(dlm_ll_info, overwrite = TRUE)
+usethis::use_data(dlm_labels, dlm_ll_info, overwrite = TRUE)
