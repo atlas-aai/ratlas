1d02d325cf32827a96a49ce6886c3480925c295b
Noelle Pablo <34638437+noellepablo@users.noreply.github.com>
2026-07-17 10:50:12 -0700

    Update R/formatting-dataframe.R
    apply formatting suggestion
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- R/formatting-dataframe.R
+++ R/formatting-dataframe.R
@@ -325,7 +325,9 @@ pad_prop <- function(x, digits, output = NULL, ...) {
 #' @rdname padding
 pad_corr <- function(x, digits, output = NULL, ...) {
   if (!is.numeric(x)) {
-    cli::cli_abort("{.arg x} must be a numeric vector, not {.obj_type_friendly x}.")
+    cli::cli_abort(
+      "{.arg x} must be a numeric vector, not {.obj_type_friendly x}."
+    )
   }
   check_number_whole(digits, min = 1)
   output <- check_output(output)
